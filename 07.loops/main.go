58627717aefe5425b8831e88bd96e8fa1e360065
Amin Abdisamad <theaminux@gmail.com>
2018-09-21 02:00:40 +0300

    added loops

---
+++ 07.loops/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	// for i := 1; i <= 10; i++ {
+	// 	fmt.Printf("Number %d\n", i)
+	// }
+	fmt.Println(fizzBuzz(100))
+}
+
+func fizzBuzz(n int) int {
+	// if n / 3 = 0 if n/5 = 0
+	for i := 1; i < n; i++ {
+
+		if i%15 == 0 {
+			fmt.Println("FizzBuzz")
+		} else if i%3 == 0 {
+			fmt.Println("Fizz")
+		} else if i%5 == 0 {
+			fmt.Println("Buzz")
+		} else {
+			fmt.Println(i)
+		}
+
+	}
+	return 0
+}
