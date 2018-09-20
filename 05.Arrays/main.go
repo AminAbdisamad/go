e1ecb481b5ee008d37f03ef6998e18aa87a15baf
Amin Abdisamad <theaminux@gmail.com>
2018-09-21 00:00:48 +0300

    Added slices

--- 05.Arrays/main.go
+++ 05.Arrays/main.go
@@ -5,12 +5,24 @@ import (
 )
 
 func main() {
-	// Arrays - in Go arrays should be specified
+	// Arrays - in Go arrays size should be specified before using it
 	var arr [4]string
 	arr[0] = "Orange"
 	arr[1] = "Blue"
 	arr[2] = "Green"
 	arr[3] = "Apple"
-	fmt.Println(arr)
+	Users := [4]string{"Hassan", "Mohamed", "Hussien", "Ali"}
+	fmt.Println(Users)
+
+	// Slices - Extended arrays
+	people := []string{"Ali", "Haji", "Hassan", "Mohamed", "Hussien"}
+	//Length
+	fmt.Println(len(people))
+	//Range
+	fmt.Println(people[1:4])
+
+	// userInfo := []string{"Mohamed Amin"}
+
+	// fmt.Println("%T\n", people)
 
 }
