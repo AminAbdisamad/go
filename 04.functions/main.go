71551700c1f2acd045d6a79cba8f5eb1a0b815d6
Amin Abdisamad <theaminux@gmail.com>
2018-09-20 20:59:12 +0300

    Added arrays

--- 04.functions/main.go
+++ 04.functions/main.go
@@ -6,7 +6,13 @@ import (
 
 func main() {
 	fmt.Println(greeting("Aminux"))
+	fmt.Println(addTwoN(4, 8))
 }
+
 func greeting(name string) string {
-	return "Hello" + name
+	return "Hello " + name
+}
+
+func addTwoN(a, b int) int {
+	return a + b
 }
---
+++ 05.Arrays/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// Arrays - in Go arrays should be specified
+	var arr [4]string
+	arr[0] = "Orange"
+	arr[1] = "Blue"
+	arr[2] = "Green"
+	arr[3] = "Apple"
+	fmt.Println(arr)
+
+}
