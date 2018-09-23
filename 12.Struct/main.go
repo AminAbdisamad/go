ce94f087acac28d6529a1d525f968840d307d0bb
Amin Abdisamad <theaminux@gmail.com>
2018-09-23 11:41:29 +0300

    Added getMarried()

--- 12.Struct/main.go
+++ 12.Struct/main.go
@@ -6,19 +6,21 @@ import (
 )
 
 func main() {
-	customer1 := Customers{firstName: "hassan", lastName: "Amin", country: "Somalia", gender: "M", address: "Godey", phone: "555-667-433", age: 20}
-	fmt.Println(customer1)
-	fmt.Println(customer1.firstName)
+	customer1 := Customers{firstName: "hassan", lastName: "Amin", country: "Somalia", status: "Single", gender: "M", address: "Godey", phone: "555-667-433", age: 20}
+	// fmt.Println(customer1)
+	// fmt.Println(customer1.firstName)
 	customer1.firstName = "Ali"
-	fmt.Println(customer1.firstName)
+	// fmt.Println(customer1.firstName)
+	customer1.birthday()
+	customer1.getMarried()
 	fmt.Println(customer1.greeting())
 
 }
 
 // strucks are more like classes in other PL.
 type Customers struct {
-	firstName, lastName, gender, address, phone, country string
-	age                                                  int
+	firstName, lastName, gender, address, phone, country, status string
+	age                                                          int
 }
 
 // Methods - we 2 kinds 1- value receivers and pointer receiver
@@ -26,5 +28,16 @@ type Customers struct {
 // Method - value reciever
 
 func (c Customers) greeting() string {
-	return "Hi, My full Name is " + c.firstName + c.lastName + "I'm " + strconv.Itoa(c.age) + " Years old from " + c.country
+	return "Hi, My  Name is " + c.firstName + " " + c.lastName + " I'm " + strconv.Itoa(c.age) + " Years old " + c.status + " from " + c.country
+}
+
+// Method - Pointer reciever
+func (c *Customers) birthday() {
+	c.age++
+}
+
+// Pointer receiver method
+func (c *Customers) getMarried() {
+	c.status = "Married"
+
 }
