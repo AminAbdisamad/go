41be8b71fc470b065a64cf8219f26980885bb58f
Amin Abdisamad <theaminux@gmail.com>
2018-09-23 11:19:31 +0300

    Added Method receiver

--- 12.Struct/main.go
+++ 12.Struct/main.go
@@ -2,24 +2,23 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
 func main() {
-	customer1 := Customers{firstName: "hassan", lastName: "Amin", gender: "M", address: "Godey", phone: "555-667-433", age: 20}
+	customer1 := Customers{firstName: "hassan", lastName: "Amin", country: "Somalia", gender: "M", address: "Godey", phone: "555-667-433", age: 20}
 	fmt.Println(customer1)
 	fmt.Println(customer1.firstName)
 	customer1.firstName = "Ali"
 	fmt.Println(customer1.firstName)
 	fmt.Println(customer1.greeting())
-	a := A{i: 2, j: 3}
 
-	fmt.Println(a.sumFields(10))
 }
 
 // strucks are more like classes in other PL.
 type Customers struct {
-	firstName, lastName, gender, address, phone string
-	age                                         int
+	firstName, lastName, gender, address, phone, country string
+	age                                                  int
 }
 
 // Methods - we 2 kinds 1- value receivers and pointer receiver
@@ -27,14 +26,5 @@ type Customers struct {
 // Method - value reciever
 
 func (c Customers) greeting() string {
-	return "Hi, " + c.firstName + " Welcome to Golang"
-}
-
-type A struct {
-	i int
-	j int
-}
-
-func (a A) sumFields(k int) int {
-	return a.i + a.j + k
+	return "Hi, My full Name is " + c.firstName + c.lastName + "I'm " + strconv.Itoa(c.age) + " Years old from " + c.country
 }
