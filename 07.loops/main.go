77484d04ab1904e82df4b100a69b995cda5c7f04
Amin Abdisamad <theaminux@gmail.com>
2021-01-08 02:43:05 +0300

    Update main.go

--- 07.loops/main.go
+++ 07.loops/main.go
@@ -12,6 +12,13 @@ func main() {
 	sum := 1
 	for ; sum < 1000; { sum += sum }
 	fmt.Println(sum)
+	
+	// For is Go's "while"
+	sum := 1
+	for sum < 1000 {
+		sum += sum
+	}
+	fmt.Println(sum)
 }
 }
 
